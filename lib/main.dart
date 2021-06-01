3473939c8179ae13c6506737c6086dc5c0d7fd6a
Diyanto TI <diyanto2911@gmail.com>
2021-06-01 16:35:54 +0700

    fix

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'screens/landing_page.dart';
+import 'notifier/auth_notifier.dart';
+import 'notifier/food_notifier.dart';
+import 'screens/login_signup_page.dart';
+
+void main() {
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (_) => AuthNotifier(),
+      ),
+      ChangeNotifierProvider(
+        create: (_) => FoodNotifier(),
+      ),
+    ],
+    child: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Food Lab',
+      theme: ThemeData(
+        fontFamily: 'Montserrat',
+        primaryColor: Color.fromRGBO(255, 63, 111, 1),
+      ),
+      home: Scaffold(
+        body: LoginPage(),
+      ),
+    );
+  }
+}
